2f687e7def5bd3b68fa8cc343ba4e1048f45ee7a
Jack Liu <Jack.Liu@dewr.gov.au>
2024-08-29 20:36:59 +1000

    call external  API

--- src/App.vue
+++ src/App.vue
@@ -14,8 +14,8 @@ export default {
     try {
       //const aaa = await (await fetch("/api/HttpTriggerTest?name=test")).json();
       const res = await fetch("/api/HttpTriggerTest?name=test");
-      const data = await res.json();
       const text = await res.text();
+      //const data = await res.json();
       this.message = res.status + "-" + res.statusText;
       this.message= text;
     }
