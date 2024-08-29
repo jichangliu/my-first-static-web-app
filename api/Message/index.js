54942d4d390e527e42b164d042f63d41538a0979
Jack Liu <Jack.Liu@dewr.gov.au>
2024-08-29 14:43:40 +1000

    feat: add API

--- api/Message/index.js
+++ api/Message/index.js
@@ -1,5 +1,6 @@
 module.exports = async function (context, req) {
+    const connectionString = process.env.DATABASE_CONNECTION_STRING;
     context.res.json({
-        text: "Hello from the API"
+        text: "connectionString in API:"+connectionString
     });
 };
